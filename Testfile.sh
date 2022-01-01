ff955128dc554103a4408822021aa9340c32ba87
fkhonjoosh <fkhonjoosh@gmail.com>
2022-01-01 18:20:20 +0100

    first commit Testfile.sh

--- Testfile.sh
+++ Testfile.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # Dateitest
 PFADNAME="C:\Users\Feri\mypipline\jenkinsdemo"
+PFAD_NAME="C:\Users\Feri\Downloads"
+ZIEL_DIR="deploy"
+
+
+
 if [ -f "$PFADNAME\readme.txt" ]
   then
     echo "Die Datei readme.txt ist da."
+    cd "$PFAD_NAME"
+    mkdir "$ZIEL_DIR"
+    cp  "$PFADNAME\readme.txt" "$PFAD_NAME\"$ZIEL_DIR\"readme.txt"
+
   elif [ -d "$PFADNAME\readme.txt" ]
     then
       echo "readme.txt ist ein Verzeichnis"
